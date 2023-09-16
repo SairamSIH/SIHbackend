7df8a12a186c9e8442d12963398e041c05f8121b
SairamSIH <144242209+SairamSIH@users.noreply.github.com>
2023-09-16 15:20:11 +0000

    redo

--- DataMan/models.py
+++ DataMan/models.py
@@ -4,11 +4,6 @@ def create_supabase_folder(unique_id):
     url = 'https://zwxpudmuwltonzohjbse.supabase.co'
     key= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3eHB1ZG11d2x0b256b2hqYnNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwOTE1NjMsImV4cCI6MjAwOTY2NzU2M30.Gk7IHFmxLj6cnr_3g1o8dv0HvodDihm2oCNG-Ica3Mw'
     clients = supabase.Client(url, key)
-    try:
-        with open("supabasetry.txt","x") as d:
-            pass 
-    except :
-        pass
     vid_res = clients.storage.from_('sihbucketbackend').upload(f'{unique_id}/Videos/supabasetry.txt', file='supabasetry.txt')
     docs_res = clients.storage.from_('sihbucketbackend').upload(f'{unique_id}/Documents/supabasetry.txt', file='supabasetry.txt')
 
