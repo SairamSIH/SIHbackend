31c83c405f4483a59b371cf30cc554e2f8334b92
SairamSIH <144242209+SairamSIH@users.noreply.github.com>
2023-09-16 14:32:34 +0000

    only supabase.txt

--- DataMan/models.py
+++ DataMan/models.py
@@ -5,8 +5,8 @@ def create_supabase_folder(unique_id):
     url = 'https://zwxpudmuwltonzohjbse.supabase.co'
     key= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3eHB1ZG11d2x0b256b2hqYnNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwOTE1NjMsImV4cCI6MjAwOTY2NzU2M30.Gk7IHFmxLj6cnr_3g1o8dv0HvodDihm2oCNG-Ica3Mw'
     clients = supabase.Client(url, key)
-    vid_res = clients.storage.from_('sihbucketbackend').upload(f'{unique_id}/Videos/supabasetry.txt', file=os.path.join('DataMan', 'supabasetry.txt'))
-    docs_res = clients.storage.from_('sihbucketbackend').upload(f'{unique_id}/Documents/supabasetry.txt', file=os.path.join('DataMan', 'supabasetry.txt'))
+    vid_res = clients.storage.from_('sihbucketbackend').upload(f'{unique_id}/Videos/supabasetry.txt', file='supabasetry.txt')
+    docs_res = clients.storage.from_('sihbucketbackend').upload(f'{unique_id}/Documents/supabasetry.txt', file='supabasetry.txt')
 
 def retrieve_supabase_folder(UniqueID):
     url = 'https://zwxpudmuwltonzohjbse.supabase.co'
