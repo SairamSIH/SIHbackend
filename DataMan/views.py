31cb97cd0e36bca94a816421148b49ccfe47b4c7
SairamSIH <144242209+SairamSIH@users.noreply.github.com>
2023-09-18 11:48:19 +0000

    bug fix for InMemoryUploadedFile

--- DataMan/views.py
+++ DataMan/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import viewsets
 from .models import mainUserCentral
 from .serializers import MainUserCentralSerializer
